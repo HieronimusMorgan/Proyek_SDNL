67a5e1803098976acbc0eb4fea4ac0f8f2d5c2ec
Ryan <Ryan@DESKTOP-91QBCFL>
2019-05-07 21:57:23 +0700

    2.2

--- src/view/Kamusku.java
+++ src/view/Kamusku.java
@@ -50,6 +50,11 @@ public class Kamusku extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        menuFile = new javax.swing.JMenu();
+        menuDaftar = new javax.swing.JMenuItem();
+        menuTentang = new javax.swing.JMenuItem();
+        menuKeluar = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -58,7 +63,7 @@ public class Kamusku extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Kamus Jawa");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 30, 240, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 30, 240, -1));
 
         indoText.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         indoText.addActionListener(new java.awt.event.ActionListener() {
@@ -66,7 +71,7 @@ public class Kamusku extends javax.swing.JFrame {
                 indoTextActionPerformed(evt);
             }
         });
-        getContentPane().add(indoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 140, 243, 40));
+        getContentPane().add(indoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 140, 243, 40));
 
         cariButton.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         cariButton.setText("Cari");
@@ -75,15 +80,15 @@ public class Kamusku extends javax.swing.JFrame {
                 cariButtonActionPerformed(evt);
             }
         });
-        getContentPane().add(cariButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 190, -1, -1));
+        getContentPane().add(cariButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 190, -1, -1));
 
         ngokoText.setEditable(false);
         ngokoText.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        getContentPane().add(ngokoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 290, 242, 35));
+        getContentPane().add(ngokoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 280, 242, 35));
 
         kramaText.setEditable(false);
         kramaText.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        getContentPane().add(kramaText, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 370, 243, 35));
+        getContentPane().add(kramaText, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 370, 243, 35));
 
         bersihButton.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         bersihButton.setText("Clean");
@@ -92,26 +97,51 @@ public class Kamusku extends javax.swing.JFrame {
                 bersihButtonActionPerformed(evt);
             }
         });
-        getContentPane().add(bersihButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 430, -1, -1));
+        getContentPane().add(bersihButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 420, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Javanese Text", 1, 24)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Indonesia");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 90, -1, -1));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 90, -1, -1));
 
         jLabel3.setFont(new java.awt.Font("Javanese Text", 1, 24)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Ngoko Lugu");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 250, -1, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 230, -1, -1));
 
         jLabel4.setFont(new java.awt.Font("Javanese Text", 1, 24)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Krama Inggil");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 330, -1, -1));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 320, -1, -1));
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/free-wayang-vector.png"))); // NOI18N
         jLabel5.setText("jLabel5");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(-60, 0, 460, 500));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(-80, -10, 480, 510));
+
+        menuFile.setText("File");
+
+        menuDaftar.setText("Daftar");
+        menuFile.add(menuDaftar);
+
+        menuTentang.setText("Tentang");
+        menuTentang.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuTentangActionPerformed(evt);
+            }
+        });
+        menuFile.add(menuTentang);
+
+        menuKeluar.setText("Keluar");
+        menuKeluar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuKeluarActionPerformed(evt);
+            }
+        });
+        menuFile.add(menuKeluar);
+
+        jMenuBar1.add(menuFile);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -144,6 +174,15 @@ public class Kamusku extends javax.swing.JFrame {
         ngokoText.setText("");
         kramaText.setText("");
     }//GEN-LAST:event_bersihButtonActionPerformed
+
+    private void menuTentangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuTentangActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menuTentangActionPerformed
+
+    private void menuKeluarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuKeluarActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_menuKeluarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -189,7 +228,12 @@ public class Kamusku extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JTextField kramaText;
+    private javax.swing.JMenuItem menuDaftar;
+    private javax.swing.JMenu menuFile;
+    private javax.swing.JMenuItem menuKeluar;
+    private javax.swing.JMenuItem menuTentang;
     private javax.swing.JTextField ngokoText;
     // End of variables declaration//GEN-END:variables
 }
