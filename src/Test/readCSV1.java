f30ffe98f7b08a4b8fe6fb4fa05bc95a7829d840
Ryan <Ryan@DESKTOP-91QBCFL>
2019-05-10 21:32:20 +0700

    2.3

---
+++ src/Test/readCSV1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ryan
+ */
+public class readCSV1 {
+
+    public static void main(String[] args) {
+        String fileName = "src/DataJawa.csv";
+        File file = new File(fileName);
+        try {
+            Scanner inputStream = new Scanner(file);
+//            inputStream.useDelimiter(",");
+            while (inputStream.hasNext()) {
+                String data = inputStream.next();
+               data.split(",");
+                System.out.println(data+" ");
+            }
+            inputStream.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(readCSV1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
