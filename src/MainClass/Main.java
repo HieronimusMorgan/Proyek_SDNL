1829600a05960194e31cdfc7ea56846ab65ac248
asus <asus@MORGAN>
2019-09-06 00:05:55 +0700

    Finish

---
+++ src/MainClass/Main.java
@@ -0,0 +1,11 @@
+package MainClass;
+
+import view.Kamusku;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Kamusku kamus = new Kamusku();
+        kamus.setVisible(true);
+    }
+}
